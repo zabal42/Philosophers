b35b56acc8a5694c07a67c56e57c02f3bf62c84b
Mikel Zabal <mikelzabal@MacBook-Air-de-Mikel.local>
2025-04-06 12:57:28 +0200

    terminando

--- prueba_deadlock.c
+++ prueba_deadlock.c
@@ -6,24 +6,29 @@
 /*   By: mikelzabal <mikelzabal@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 19:47:37 by mikelzabal        #+#    #+#             */
-/*   Updated: 2025/04/04 16:16:54 by mikelzabal       ###   ########.fr       */
+/*   Updated: 2025/04/06 12:29:05 by mikelzabal       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <sys/time.h>
+
 
 #define NUM_FILOSOFOS 5
 #define NUM_REPETICIONES 1
+#define TIME_TO_DIE 1000
 
+volatile int	someone_died = 0;
 pthread_mutex_t	tenedores[NUM_FILOSOFOS];
 
 typedef	struct s_filosofo
 {
-	int		id;
+	int				id;
 	pthread_mutex_t	*tenedor_izq;
 	pthread_mutex_t	*tenedor_der;
+	long			last_meal_time;
 } t_filosofo;
 
 void	esperar(const char *accion, int id, int tiempo)
@@ -31,6 +36,14 @@ void	esperar(const char *accion, int id, int tiempo)
 	printf("el filosofo %d esta %s\n", id, accion);
 	usleep(tiempo);
 }
+
+long	get_timestamp(void)
+{
+	struct timeval	tv;
+	gettimeofday(&tv, NULL);
+	return((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
 
 void	*cosas_de_filosofos(void *arg)
 {
@@ -52,6 +65,7 @@ void	*cosas_de_filosofos(void *arg)
 		pthread_mutex_lock(filo->tenedor_izq);
 		printf("Filósofo %d (impar) ha cogido el tenedor izquierdo 🍴\n", filo->id);
 	}
+	filo->last_meal_time = get_timestamp();
 	esperar("Comiendo🍜", filo->id, 500000);
 	pthread_mutex_unlock(filo->tenedor_izq);
 	pthread_mutex_unlock(filo->tenedor_der);
@@ -59,10 +73,33 @@ void	*cosas_de_filosofos(void *arg)
 	printf("El filosofo %d ha terminado de comer😎\n", filo->id);
 	return (NULL);
 }
+void *parroco(void *arg)
+{
+	t_filosofo	*filos;
+	int			i;
+	filos = (t_filosofo *)arg;
+	
+	while (1)
+	{
+		i = 0;
+		while (i < NUM_FILOSOFOS)
+		{
+			if ((get_timestamp() - filos[i].last_meal_time) > TIME_TO_DIE)
+			{
+				printf("💀 Filósofo %d ha muerto\n", filos[i].id);
+				someone_died = 1;
+				return (NULL);
+			}
+			i++;
+		}
+		usleep(1000);
+	}
+}
 
 int	main(void)
 {
 	pthread_t	hilos[NUM_FILOSOFOS];
+	pthread_t	rip;
 	t_filosofo	filosofos[NUM_FILOSOFOS];
 	int			i;
 
@@ -72,16 +109,17 @@ int	main(void)
 		pthread_mutex_init(&tenedores[i], NULL);
 		i++;
 	}
-	
 	i = 0;
 	while(i < NUM_FILOSOFOS)
 	{
 		filosofos[i].id = i + 1;
+		filosofos[i].last_meal_time = get_timestamp();
 		filosofos[i].tenedor_izq = &tenedores[i];
 		filosofos[i].tenedor_der = &tenedores[(i + 1) % NUM_FILOSOFOS];
 		pthread_create(&hilos[i], NULL, cosas_de_filosofos, &filosofos[i]);
 		i++;
 	}
+	pthread_create(&rip, NULL, parroco, filosofos);
 	i = 0;
 	while (i < NUM_FILOSOFOS)
 	{
@@ -94,7 +132,7 @@ int	main(void)
 		pthread_mutex_destroy(&tenedores[i]);
 		i++;
 	}
-	printf("todos los filosofos han terminado sus movidas. 👏👏👏\n");
+	pthread_join(rip, NULL);
 	return (0);
 }
 
